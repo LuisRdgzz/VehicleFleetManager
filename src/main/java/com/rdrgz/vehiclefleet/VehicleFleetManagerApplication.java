46bc0b93104bebaf93a6fe94cbdf84c4f9ef8de6
RamirezLuis <cobarluis7@gmail.com>
2024-11-02 12:32:03 -0600

    Starting Proyect

---
+++ src/main/java/com/rdrgz/vehiclefleet/VehicleFleetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rdrgz.vehiclefleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleFleetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleFleetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdrgz/vehiclefleet/VehicleFleetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdrgz.vehiclefleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleFleetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
